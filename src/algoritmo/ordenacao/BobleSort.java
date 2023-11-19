19c5f48062f40dbd62f970f4fe9524d96c7322ba
erickbrunopequenodossantos@outlook.com <erickbrunopequenodossantos@outlook.com>
2023-11-18 23:43:08 -0300

    corrigido

--- src/algoritmo/ordenacao/BobleSort.java
+++ src/algoritmo/ordenacao/BobleSort.java
@@ -2,23 +2,18 @@ package algoritmo.ordenacao;
 
 public class BobleSort {
 	public static void main(String[] args) {
-
 		int[] array = new int[] { 6, 3, 8, 2, 5, 10, 9, 4 };
-
-		int ax;
+		int auxialiar;
 
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array.length - 1; j++) {
-
 				if (array[j] > array[j + 1]) {
-					ax = array[j + 1];
+					auxialiar = array[j + 1];
 					array[j + 1] = array[j];
-					array[j] = ax;
-
+					array[j] = auxialiar;
 				}
 			}
 		}
-
 		System.out.println("------------------Ordenado--------------------");
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
